733fb68dd840a17e2a7ca16953045c19961a0350
kolya <kolyan78@gmail.com>
2021-11-06 22:13:47 +0300

    add something

--- task9.py
+++ task9.py
@@ -2,3 +2,16 @@
 9. На вход функция more_than_five(lst) получает список из целых чисел.
 Результатом работы функции должен стать новый список, в котором содержатся только те числа, которые больше 5 по модулю.
 '''
+import random
+
+def more_than_five(lst):
+    print(lst)
+    b = [x for x in lst if abs(x) > 5]
+    return b
+
+def gen_list(num):
+    a = [random.randint(-10, 10) for _ in list(range(num))]
+    return a
+
+a = gen_list(20)
+print (more_than_five(a))
